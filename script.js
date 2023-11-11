47c25ab3a3be6a1a70914b9ea351eee19ab294a5
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-11 11:50:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 function validEmail(str) {
   //your JS code here.
+	if (!email || email.length > 10^6) {
+        return false;
+    }
+
+    const emailParts = email.split('@');
+    if (emailParts.length !== 2) {
+        return false;
+    }
+
+    const [localPart, domain] = emailParts;
+    if (!localPart || !domain) {
+        return false;
+    }
+
+    if (localPart.length > 64) {
+        return false;
+    }
+
+    const domainParts = domain.split('.');
+    if (domainParts.length < 2) {
+        return false;
+    }
+
+    for (let i = 0; i < domainParts.length; i++) {
+        const domainPart = domainParts[i];
+        if (!domainPart || domainPart.length > 63) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 // Do not change the code below.
